e3f711ad95267c965005448d6d0f1ab9479e761a
Anshul1507 <ag1507anshul@gmail.com>
2020-09-07 23:30:32 +0530

    Added - Layouts file

---
+++ app/src/main/java/tech/anshul1507/inappreviewplaystore/SecondActivity.kt
@@ -0,0 +1,11 @@
+package tech.anshul1507.inappreviewplaystore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
